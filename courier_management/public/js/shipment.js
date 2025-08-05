2f069f11dfd345d59fb6a24e20a7106f32dfbb60
viralkansodiya <viral@fosserp.com>
2025-08-05 14:24:39 +0530

    feat: Docket printing api integration

--- courier_management/public/js/shipment.js
+++ courier_management/public/js/shipment.js
@@ -48,22 +48,24 @@ frappe.ui.form.on("Shipment", {
             });
         }
         if(!frm.is_new()){
-            frm.add_custom_button(__("Request Pickep Parcel"), ()=>{
-                if(!frm.doc.awb_number){
-                    frappe.throw("Docket No is not Generated.")
-                }
-                frappe.call({
-                    method: "courier_management.courier_management.doc_events.shipment.booking_of_shipment",
-                    args:{
-                        doc : frm.doc
-                    },
-                    callback(r){
-                        if (r.message){
-                            frm.refresh_field("shipment_id")
-                        }
+            if(frm.doc.courier_partner){
+                frm.add_custom_button(__("Request Pickep Parcel"), ()=>{
+                    if(!frm.doc.awb_number){
+                        frappe.throw("Docket No is not Generated.")
                     }
+                    frappe.call({
+                        method: "courier_management.courier_management.doc_events.shipment.booking_of_shipment",
+                        args:{
+                            doc : frm.doc
+                        },
+                        callback(r){
+                            if (r.message){
+                                frm.refresh_field("shipment_id")
+                            }
+                        }
+                    })
                 })
-            })
+            }
             if(frm.doc.shipment_id && frm.doc.awb_number){
                 frm.add_custom_button(__("Docket Print"),()=>{
                     if(!frm.doc.awb_number){
