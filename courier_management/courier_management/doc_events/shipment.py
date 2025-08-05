7193bf3dae730d64d6a6161a071f301a0211068b
viralkansodiya <viral@fosserp.com>
2025-08-05 12:34:04 +0530

    fix: added a debugging message

--- courier_management/courier_management/doc_events/shipment.py
+++ courier_management/courier_management/doc_events/shipment.py
@@ -221,7 +221,7 @@ def booking_of_shipment(doc):
                     "deliveryStn": "",  # Empty string
                     "docketNo": doc.awb_number,
                     "EWAYBILL": ewaybill_data.get("ewaybill"),
-                    "EWB_EXP_DT": str(getdate(ewaybill_data.get("valid_upto"))),
+                    "EWB_EXP_DT": '',
                     "fromPkgNo": doc.shipment_parcel[0].get("parcel_series"),
                     "goodsCode": "302",  # Static value
                     "goodsDesc": doc.description_of_content,
