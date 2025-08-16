5275b18123d95e6be2110bcd66d2c0ecb49b2267
viralkansodiya <viral@fosserp.com>
2025-08-16 15:09:37 +0530

    fix: cancellation issue

--- courier_management/courier_management/doc_events/shipment.py
+++ courier_management/courier_management/doc_events/shipment.py
@@ -571,5 +571,5 @@ def cancelle_pickup_booking(doc):
 def before_cancel(self, method):
     if not self.courier_partner:
         return
-    if not is_cancelled:
+    if not self.is_cancelled:
         frappe.throw("Shipment pickup service is not cancelled.")
